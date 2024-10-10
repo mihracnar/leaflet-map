a64a2ac53c056e6e16bfa8dd97c6b0d74feb0c3d
Mihrac <mihrac.nar@outlook.com>
2024-10-10 19:59:17 +0300

    initial commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,122 @@
+const map = L.map('map').setView([41.0082, 28.9784], 13);
+
+const cartoLightLayer = L.tileLayer('https://a.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
+    attribution: '© <a href="https://carto.com/">Carto</a>',
+    maxZoom: 22
+}).addTo(map);
+
+const googleMapsSatelliteLayer = L.tileLayer('https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}', {
+    attribution: '© <a href="https://maps.google.com/maps/about/terms/copyright/">Google Maps</a>',
+    maxZoom: 22
+});
+
+const googleMapsLayer = L.tileLayer('https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
+    attribution: '© <a href="https://maps.google.com/maps/about/terms/copyright/">Google Maps</a>',
+    maxZoom: 22
+});
+
+const googleMapsSatelliteHybridLayer = L.tileLayer('https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}', {
+    attribution: '© <a href="https://maps.google.com/maps/about/terms/copyright/">Google Maps</a>',
+    maxZoom: 22
+});
+
+const osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 22
+});
+
+// Katman kontrolünü oluşturma
+const layersControl = L.control.layers({
+    "Carto Light": cartoLightLayer,
+    "Google Maps (Uydu)": googleMapsSatelliteLayer,
+    "Google Maps (Harita)": googleMapsLayer,
+    "Google Maps (Uydu Hibrit)": googleMapsSatelliteHybridLayer,
+    "OpenStreetMap": osmLayer
+}, {}, { collapsed: false }); 
+
+// Katman kontrolünü modal içine taşıma
+layersControl.addTo(map); 
+const layerControlElement = layersControl.getContainer();
+document.getElementById('layersModalBody').appendChild(layerControlElement);
+
+
+// Konum butonu işlevi
+    // Leaflet'in locate kontrolünü gizli başlat
+const locateControl = L.control.locate({
+  position: 'topright', // Konum seçeneği
+  flyTo: true, // Konuma uçarak git
+  showPopup: false, // Popup gösterme
+  strings: { // Başlık metni
+    title: "Konumuma Git"
+  },
+  icon: 'fa fa-location-arrow', // İkon sınıfı
+  setView: true, // Haritayı konuma otomatik olarak yakınlaştırma
+  showMarker: true // Marker gösterme
+}).addTo(map);
+
+    // Locate kontrolünün butonunu gizle
+locateControl.getContainer().style.display = 'none';
+
+    // Konum butonu işlevi
+const locateButton = document.getElementById('locate-button');
+
+locateButton.addEventListener('click', function() {
+      // Locate kontrolünü çalıştır
+  locateControl.start();
+});
+
+
+const searchControl = new L.Control.Search({
+    url: 'https://nominatim.openstreetmap.org/search?format=json&q={s}',
+    jsonpParam: 'json_callback',
+    propertyName: 'display_name',
+    propertyLoc: ['lat', 'lon'],
+    marker: L.circleMarker([0, 0], { radius: 30 }),
+    autoCollapse: false,
+    autoType: false,
+    minLength: 2,
+    collapsed: false,
+    textPlaceholder: 'Konum ya da koordinat ara'
+});
+
+map.addControl(searchControl);
+
+let searchMarker; 
+
+searchControl.on('search:locationfound', function(e) {
+    if (searchMarker) {
+        map.removeLayer(searchMarker); 
+    }
+    lat = e.latlng.lat;
+    lon = e.latlng.lng;
+    searchMarker = L.circleMarker(e.latlng, { radius: 30 }).addTo(map);
+});
+
+searchControl.on('search:textentered', function(e) {
+    let inputText = e.text.trim();
+
+    const coords = inputText.split(',');
+    if (coords.length === 2 && !isNaN(coords[0]) && !isNaN(coords[1])) {
+        lat = parseFloat(coords[0]);
+        lon = parseFloat(coords[1]);
+
+        map.setView([lat, lon], 13);
+
+        if (searchMarker) {
+            map.removeLayer(searchMarker);
+        }
+        searchMarker = L.marker([lat, lon]).addTo(map);
+    }
+});
+
+// Zoom butonları işlevselliği
+const zoomInButton = document.getElementById('zoom-in-button');
+const zoomOutButton = document.getElementById('zoom-out-button');
+
+zoomInButton.addEventListener('click', function() {
+  map.zoomIn();
+});
+
+zoomOutButton.addEventListener('click', function() {
+  map.zoomOut();
+});
