4934970e52195d6ef74e4e465a5c7a009a29c4b1
Mihrac <mihrac.nar@outlook.com>
2024-10-16 12:54:52 +0300

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -40,7 +40,6 @@ const layerControlElement = layersControl.getContainer();
 document.getElementById('layersModalBody').appendChild(layerControlElement);
 
 
-// Konum butonu işlevi
 // Leaflet'in locate kontrolünü gizli başlat
 const locateControl = L.control.locate({
   position: 'topright', // Konum seçeneği
@@ -54,18 +53,17 @@ const locateControl = L.control.locate({
   showMarker: true // Marker gösterme
 }).addTo(map);
 
-    // Locate kontrolünün butonunu gizle
+// Locate kontrolünün butonunu gizle
 locateControl.getContainer().style.display = 'none';
 
-    // Konum butonu işlevi
+// Konum butonu işlevi
 const locateButton = document.getElementById('locate-button');
-
 locateButton.addEventListener('click', function() {
-      // Locate kontrolünü çalıştır
+// Locate kontrolünü çalıştır
   locateControl.start();
 });
 
-
+// Search Control
 const searchControl = new L.Control.Search({
     url: 'https://nominatim.openstreetmap.org/search?format=json&q={s}',
     jsonpParam: 'json_callback',
@@ -123,6 +121,7 @@ zoomOutButton.addEventListener('click', function() {
 
 
 
+// Konum Pini
 const blackIcon = L.icon({
     iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-black.png',
     iconSize: [25, 41],
@@ -143,7 +142,7 @@ toggleButton.classList.remove('active');
 toggleButton.setAttribute('aria-pressed', 'false');
 
 
-
+// Pin varsa buton aktif
 toggleButton.addEventListener('click', function () {
     if (toggleButton.classList.contains('active')) {
         mapDiv.style.display = 'none';
@@ -158,6 +157,7 @@ toggleButton.addEventListener('click', function () {
       }
 });
 
+// Harita üzerinde tıklama olayını dinleyin
 map.on('click', function (e) {
     const latlng = e.latlng;
 
@@ -165,45 +165,59 @@ map.on('click', function (e) {
         map.removeLayer(currentMarker);
     }
 
-    const marker = L.marker(latlng, { icon: blackIcon }).addTo(map); 
-    marker.bindPopup(`<b>Konum:</b> ${latlng.lat.toFixed(4)}, ${latlng.lng.toFixed(4)}`).openPopup();
+// Pin Oluştur
+const marker = L.marker(latlng, { icon: blackIcon }).addTo(map);
+currentMarker = marker;
 
-    currentMarker = marker;
-    toggleButton.disabled = false; // Butonu etkinleştir
+// Popup ile Pini ilişkilendir
+marker.bindPopup(`<b>Konum:</b> ${latlng.lat.toFixed(4)}, ${latlng.lng.toFixed(4)}`).openPopup();
 
 
-    marker.on('popupclose', function () {
-        map.removeLayer(marker);
+// Pin VarsaButonu etkinleştir
+toggleButton.disabled = false; 
+
+// Formdaki konum girdisini güncelleyin
+document.getElementById('noteLocation').value = `Lat: ${latlng.lat.toFixed(4)}, Lon: ${latlng.lng.toFixed(4)}`;
+
+
+// Gizli giriş alanlarını güncelleyin
+document.getElementById('y_koordinat').value = latlng.lat;
+document.getElementById('x_koordinat').value = latlng.lng;
+
+
+//Popup, Buton, 
+marker.on('popupclose', function () {
+    map.removeLayer(marker);
         currentMarker = null;
-        toggleButton.disabled = true; // Butonu devre dışı bırak
-        toggleButton.classList.remove('active');
-        toggleButton.setAttribute('aria-pressed', 'false');
-      });
+    toggleButton.disabled = true; // Butonu devre dışı bırak
+    toggleButton.classList.remove('active');
+    toggleButton.setAttribute('aria-pressed', 'false');
+    });
 });
  
+
+ // Not ekleme formu
+ const noteForm = document.getElementById('noteForm');
+ const noteLocationInput = document.getElementById('noteLocation');
+
+
 
  // Supabase ayarları
  const supabaseUrl = 'https://fyprykalancslcxodoge.supabase.co';
  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5cHJ5a2FsYW5jc2xjeG9kb2dlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY5MDAwNTEsImV4cCI6MjA0MjQ3NjA1MX0.RT7IOmOh_iVrmUexXXRrv8f02NJY2k_i__1W1kCMsDo';
  const supabase = supabase.createClient(supabaseUrl, supabaseKey);
 
-
- // Not ekleme formu
- const noteForm = document.getElementById('noteForm');
- const noteLocationInput = document.getElementById('noteLocation');
-
  noteForm.addEventListener('submit', async function (e) {
-     e.preventDefault();
-     // Form verilerini işle (örneğin, not içeriğini al)
-     const noteContent = document.getElementById('noteContent').value;
-     const noteLocation = noteLocationInput.value;
-     // Konum verilerini ayırma
-     const [lat, lng] = noteLocation.split(',').map(parseFloat);
-
-     // Supabase'e veri gönderme
-     const { data, error } = await supabase
-     .from('notes')
-     .insert([{ content: noteContent, location_y: lat, location_x: lng }]);
+    e.preventDefault();
+    // Form verilerini işle (örneğin, not içeriğini al)
+    const noteContent = document.getElementById('noteContent').value;
+    const y_koordinat = document.getElementById('y_koordinat').value;
+    const x_koordinat = document.getElementById('x_koordinat').value;
+  
+    // Supabase'e veri gönderme
+    const { data, error } = await supabase
+    .from('notes')
+    .insert([{ content: noteContent, location_y: y_koordinat, location_x: x_koordinat }]);
 
 
     // Örneğin, hata durumunda kullanıcıya bir mesaj gösterebilir veya başarılı işlemde bir teyit mesajı gösterebilirsiniz
